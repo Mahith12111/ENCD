0b3ac491bd7941110e1f81e968cacc220e2f9176
mradulkumar-glitch <84231956+mradulkumar-glitch@users.noreply.github.com>
2022-04-20 06:34:42 +0530

    Update incoming_message_fn.py

--- bot/plugins/incoming_message_fn.py
+++ bot/plugins/incoming_message_fn.py
@@ -17,8 +17,16 @@ from bot import (
   data,
   app  
 )
-from bot.helper_funcs.ffmpeg *
+
+from bot.helper_funcs.ffmpeg import (
+  convert_video,
+  media_info,
+  take_screen_shot,
+  out_put_file_name
+)
+
 ## COWARDS ##
+
 from bot.helper_funcs.display_progress import (
   progress_for_pyrogram,
   TimeFormatter,
